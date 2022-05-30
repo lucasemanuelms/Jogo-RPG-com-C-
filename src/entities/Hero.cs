6e8f68dccf726570e10000a036d2916bc0482d54
LucasDev <lucas.ufc1310@gmail.com>
2022-05-30 10:59:40 -0300

    Foi criado o método Attack, que vai dizer quando o personagem atacou. Esse método tem a palavra virtual, meio que ela diz que qualquer filho dessa classe Hero vai poder modificar esse método.

--- src/entities/Hero.cs
+++ src/entities/Hero.cs
@@ -15,5 +15,9 @@ namespace JogoRPG
         public override string ToString(){
             return "Character data" + "\n" + "Name: " + this.Name + "\n" + "Level: " + this.Level + "\n" + "Hero type: " + this.HeroType;
         }
+
+        public virtual string Attack(){
+            return this.Name + "atacou usando (aqui vai ser dito com o que o personagem atacou).";
+        }
     }
 }
